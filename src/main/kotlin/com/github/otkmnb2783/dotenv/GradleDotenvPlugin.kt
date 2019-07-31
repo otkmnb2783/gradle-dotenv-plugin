272b34a205ff1911362133487d7dc9b736ede100
Manabu OHTAKE <manabu2783@hotmail.com>
2019-07-31 14:02:44 +0900

    added: logging

--- src/main/kotlin/com/github/otkmnb2783/dotenv/GradleDotenvPlugin.kt
+++ src/main/kotlin/com/github/otkmnb2783/dotenv/GradleDotenvPlugin.kt
@@ -1,15 +1,19 @@
 package com.github.otkmnb2783.dotenv
 
-import org.gradle.api.Project
 import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.api.logging.Logging
 
 class GradleDotenvPlugin: Plugin<Project> {
+    private val logger = Logging.getLogger(GradleDotenvPlugin::class.java)
     override fun apply(project: Project) {
+        logger.info("gradle dotenv plugin applying")
         // Register a task
         project.tasks.register("greeting") { task ->
             task.doLast {
                 println("Hello from plugin 'com.github.otkmnb2783.dotenv'")
             }
         }
+        logger.info("gradle dotenv plugin applied")
     }
 }
