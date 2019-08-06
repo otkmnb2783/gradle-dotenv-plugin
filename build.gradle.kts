01a97ec1030c48cbd92f661fa1d06e9998923a7a
Manabu OHTAKE <manabu2783@hotmail.com>
2019-08-06 23:05:25 +0900

    added: plugin publish

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,12 +5,12 @@
  * For more details take a look at the Writing Custom Plugins chapter in the Gradle
  * User Manual available at https://docs.gradle.org/5.5/userguide/custom_plugins.html
  */
-
 plugins {
     // Apply the Java Gradle plugin development plugin to add support for developing Gradle plugins
     `java-gradle-plugin`
     // Apply the Kotlin JVM plugin to add support for Kotlin.
     id("org.jetbrains.kotlin.jvm").version("1.3.31")
+    id("com.gradle.plugin-publish") version "0.10.1"
 }
 
 repositories {
@@ -33,6 +33,59 @@ gradlePlugin {
     val dotenv by plugins.creating {
         id = "com.github.otkmnb2783.dotenv"
         implementationClass = "com.github.otkmnb2783.dotenv.GradleDotenvPlugin"
+    }
+}
+
+pluginBundle {
+    // These settings are set for the whole plugin bundle
+    website = "https://github.com/otkmnb2783/gradle-dotenv-plugin"
+    vcsUrl = "https://github.com/otkmnb2783/gradle-dotenv-plugin"
+    // tags and description can be set for the whole bundle here, but can also
+    // be set / overridden in the config for specific plugins
+    description = "This plugin loads the dotenv file so that it can be referenced from build.gradle."
+
+    // The plugins block can contain multiple plugin entries.
+    //
+    // The name for each plugin block below (greetingsPlugin, goodbyePlugin)
+    // does not affect the plugin configuration, but they need to be unique
+    // for each plugin.
+
+    // Plugin config blocks can set the id, displayName, version, description
+    // and tags for each plugin.
+
+    // id and displayName are mandatory.
+    // If no version is set, the project version will be used.
+    // If no tags or description are set, the tags or description from the
+    // pluginBundle block will be used, but they must be set in one of the
+    // two places.
+
+    (plugins) {
+
+        "dotenv" {
+            // id is captured from java-gradle-plugin configuration
+            displayName = "Gradle Dotenv plugin"
+            tags = listOf("gradle", "tags", "per", "plugin")
+            version = "0.0.1"
+        }
+
+    }
+
+    // Optional overrides for Maven coordinates.
+    // If you have an existing plugin deployed to Bintray and would like to keep
+    // your existing group ID and artifact ID for continuity, you can specify
+    // them here.
+    //
+    // As publishing to a custom group requires manual approval by the Gradle
+    // team for security reasons, we recommend not overriding the group ID unless
+    // you have an existing group ID that you wish to keep. If not overridden,
+    // plugins will be published automatically without a manual approval process.
+    //
+    // You can also override the version of the deployed artifact here, though it
+    // defaults to the project version, which would normally be sufficient.
+
+    mavenCoordinates {
+        groupId = "com.github.otkmnb2783"
+        artifactId = "dotenv-plugins"
         version = "0.0.1"
     }
 }
